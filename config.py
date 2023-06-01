0ff7dccd0832bbace4cb5553d0d111e6156da12c
Andrew <66161226+AndrewA7@users.noreply.github.com>
2023-06-01 10:47:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-TELEGRAM_TOKEN = "6120513280:AAE1VOUubsbF03CXfYfVR_PubcevTU3yk4w"
-CHAT_ID = "-1001864207689"
+TELEGRAM_TOKEN = ""
+CHAT_ID = ""
 
-LINK_TO_JIRA = "https://ajaxsystems.atlassian.net"
-LOGIN = "dzhylaii.a@ajax.systems"
-JIRA_API_TOKEN = "ATATT3xFfGF0rYm4gLqVPsLaQE4sNRP4Osmnt--Rp0IJr18RJwS1saH85qJ9jaQ8jdjHA21-" \
-                 "rU66M_vKhCyLuwn9QY0_gLjW136Sld_hZW8I23BZB7I9MXy6--cxzPitz7gtbRt7jZzSilhr" \
-                 "OojBruBdDe3lnwc_KqyQpHmfpgAoBnviNb6l7-0=D651DCAE"
+LINK_TO_JIRA = ""
+LOGIN = ""
+JIRA_API_TOKEN = ""
 
-CURRENT_STREAM = "INTRUSION"
-YOUR_JIRA_BOARD = 434
+CURRENT_STREAM = ""
+YOUR_JIRA_BOARD = 0
 TASK_TYPE = "Bug"
 TASK_STATUS = ("Open", "Backlog")
 
